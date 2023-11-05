c74c1c8bdaddb25902d8aee6783f702f9ac1ffd6
difficu1t <mishenko_artem4565@mail.ru>
2023-11-05 20:10:44 +0300

    commit

---
+++ src/components/CustomThemeProvider/index.tsx
@@ -0,0 +1,19 @@
+import { useMemo } from 'react'
+import { getTheme } from "@/utils/theme"
+import { ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material"
+import { RootState } from '@/store'
+import { useSelector } from 'react-redux'
+
+const CustomeThemeProvider = ({children}: {children: React.ReactNode}) => {
+  const { mode } = useSelector((state: RootState) => state.mode)
+  const theme = createTheme(getTheme(mode));
+
+  return (
+    <ThemeProvider theme={theme}>
+        {children}
+    </ThemeProvider>
+  )
+}
+
+export default CustomeThemeProvider
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,7 +1,98 @@
-import {createTheme} from "@mui/material";
+import { PaletteMode } from "@mui/material"
+import { ThemeMode } from "@/common/enums/themeMode"
 
-export const theme = createTheme({
-  typography: {
-    fontFamily: '"Comfortaa", cursive',
-  },
-});
+const theme = new Map([
+  [ThemeMode.DarkTheme, {
+    typography: {
+      fontFamily: '"Comfortaa", cursive',
+    },
+    palette: {
+      mode: ThemeMode.DarkTheme,
+      primary: {
+        main: "#7c39ee",
+      },
+      background: {
+        default: "#24242C",
+        paper: "#24242C",
+      },
+      secondary: {
+        main: "#ffffff",
+      },
+    },
+    components: {
+      MuiInputBase: {
+        styleOverrides: {
+          root: {
+            "&.Mui-disabled": {
+              backgroundColor: "#e4e4e4",
+            },
+          },
+        },
+      },
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "& .MuiInputBase-input": {
+              color: "white",
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "#7e7e7e",
+              },
+              "&:hover fieldset": {
+                borderColor: "#7c39ee",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "#7c39ee",
+              },
+            },
+          },
+        },
+      },
+    },
+  }],
+
+  [ThemeMode.LightTheme, {
+    typography: {
+      fontFamily: '"Comfortaa", cursive',
+    },
+    palette: {
+      mode: ThemeMode.LightTheme,
+    },
+    components: {
+      MuiInputBase: {
+        styleOverrides: {
+          root: {
+            "&.Mui-disabled": {
+              backgroundColor: "#e4e4e4",
+            },
+          },
+        },
+      },
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "& .MuiInputBase-input": {
+              color: "white",
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "#7e7e7e",
+              },
+              "&:hover fieldset": {
+                borderColor: "#7c39ee",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "#7c39ee",
+              },
+            },
+          },
+        },
+      },
+    },
+  }]
+])
+
+export const getTheme = (mode: ThemeMode) => {
+  return theme.get(mode)
+}
